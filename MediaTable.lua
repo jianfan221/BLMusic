07929efe8f825ca88b9ec6de9d98a7f6c1ec733c
arciay <32655163@qq.com>
2026-07-28 19:16:18 +0800

    添加 Samurai Heart.ogg（魔神坛斗士）到开始音频列表

--- MediaTable.lua
+++ MediaTable.lua
@@ -29,6 +29,7 @@ ns.start = {
     { path = "ikun.mp3", name = "ikun" },
     { path = "manboNomore.mp3", name = "manboNomore" },--提供者Sins
     { path = "Sway to My Beat in Cosmos.mp3", name = T("在银河中孤独摇摆", "Sway to My Beat in Cosmos") },--提供者lzy
+    { path = "Samurai Heart.ogg", name = T("魔神坛斗士", "Samurai Heart") },--提供者Nathan
 }
 --中文开始音频仅对中文用户显示
 if locale then
